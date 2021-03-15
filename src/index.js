a38f4845e4427bb04b798c060362fe3ce5f56ba8
Юрій Пристай <yurayprystay@gmail.com>
2021-03-15 17:10:10 +0200

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let countStr = 0;
+    let stage;
+    str = str.split('');
+    while (countStr < str.length-1) {
+        stage = false;
+        for (let item of bracketsConfig) {
+            if (item[0] === str[countStr] && item[1] === str[countStr+1]) {
+                str.splice(countStr, 2);
+                stage = true;
+                countStr = 0;
+                break;
+            }
+        }
+        if ((!stage && countStr >= str.length-2) || str.length % 2 !== 0) {
+            return false;
+        } else if (!stage && countStr < str.length-2) {
+            countStr++;
+        }
+        if (str.length === 0) {
+            return true;
+        }
+    }
 }
